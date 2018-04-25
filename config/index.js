6221f87389d25d5baa323af0a4cde4bc83a35120
ygjack <ygjack414@hotmail.com>
2018-04-25 13:14:58 +0800

    update

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'threejs-sphere-examples',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
